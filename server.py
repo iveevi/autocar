c1edbc4bb519f70e6c2760c682c84473aa4457f5
Venkster123 <veskimo123@gmail.com>
2021-06-21 17:42:57 -0700

    Adding keyboard input

--- server.py
+++ server.py
@@ -4,6 +4,9 @@ from flask import request
 
 app = Flask(__name__)
 
+drive = 0
+steer = 0
+
 @app.route('/')
 def control():
     return render_template('control.html')
@@ -16,6 +19,13 @@ def code():
 def get_names():
    if request.method == 'POST':
         names = request.get_json()
-        print('Steer mode: ' + str(names['steer']))
-        print('Drive mode: ' + str(names['drive']))
+        steer = names['steer']
+        drive = names['drive']
+        # print('Steer mode: ' + str(names['steer']))
+        # print('Drive mode: ' + str(names['drive']))
         return '', 200
+
+''' while True:
+        print("Input:")
+        print(f"\tdrive = {drive}")
+        print(f"\tsteer = {steer}") '''
--- static/control.js
+++ static/control.js
@@ -1,11 +1,19 @@
 var steer = 0
 var drive = 0
 
-$('#dummy').click(function () {
-        console.log('Sending...');
-        console.log(steer);
-        console.log(drive);
-        
+document.addEventListener('keydown', function (event) {
+        // Use switch
+        if (event.keyCode == 37)
+                steer = -1;
+        if (event.keyCode == 38)
+                drive = 1;
+        else if (event.keyCode == 39)
+                steer = 1;
+        else if (event.keyCode == 40)
+                drive = -1;
+        else
+                return;
+
         $.ajax({
                 type: "POST",
                 contentType: "application/json;charset=utf-8",
