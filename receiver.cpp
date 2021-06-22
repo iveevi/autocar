568f1985a54d9937222d67b14c1f9f1a5b884c23
vedavamadathil <vesion4690@gmail.com>
2021-06-21 19:53:02 -0700

    Updated receiver

--- receiver.cpp
+++ receiver.cpp
@@ -15,7 +15,7 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	int server_fd, new_socket, valread;
-	struct sockaddr_in address;
+	sockaddr_in address;
 	int opt = 1;
 	int addrlen = sizeof(address);
 
@@ -33,25 +33,24 @@ int main(int argc, char const *argv[])
 		perror("setsockopt");
 		exit(EXIT_FAILURE);
 	}
+	
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons( PORT );
+	address.sin_port = htons(PORT);
 
 	// Forcefully attaching socket to the port 8080
-	if (bind(server_fd, (struct sockaddr *)&address, 
-				sizeof(address))<0)
-	{
+	if (bind(server_fd, (sockaddr *) &address, sizeof(address)) < 0) {
 		perror("bind failed");
 		exit(EXIT_FAILURE);
 	}
+
 	if (listen(server_fd, 3) < 0)
 	{
 		perror("listen");
 		exit(EXIT_FAILURE);
 	}
 	
-	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-					(socklen_t*)&addrlen))<0)
+	if ((new_socket = accept(server_fd, (sockaddr *) &address, (socklen_t*) &addrlen))<0)
 	{
 		perror("accept");
 		exit(EXIT_FAILURE);
